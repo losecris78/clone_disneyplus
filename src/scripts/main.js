b2bba53748e395a31173eca3354d7d28618c438d
Cristian Lopez <losecris@gmail.com>
2024-02-03 20:45:29 -0300

    Finalizacao do site responsivo e uglify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button');
     const questions = document.querySelectorAll('[data-faq-question]');
-//se tienen que usar corchetes para seleccionar un atributo
-
+    //se tienen que usar corchetes para seleccionar un atributo
+    const heroSection = document.querySelector('.hero');
+    const clientHeight = heroSection.clientHeight;
+    //para seleccionar la altura usamos la propiedad clientHeight
+window.addEventListener('scroll', function(){
+    const posicaoAtual= window.scrollY;
+    if (posicaoAtual < clientHeight){
+        hideHeaderElements();
+    } else{
+        showHeaderElements();
+    }
+});
+    //Section shows, programacao das abas
     for (let i= 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+    //section faq, abre e fecha resposta
     for(let i = 0; i < questions.length ; i++){
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
@@ -39,4 +51,12 @@ function escondeTodasAbas(){
     for (let i = 0; i < tabsContainer.length; i++){
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
+}
+function showHeaderElements(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+function hideHeaderElements(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
 }
